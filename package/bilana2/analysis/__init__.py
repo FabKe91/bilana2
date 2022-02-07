4979322d6f2ef48543bfd4b4dc08e974c876bef1
f_kell07 <f_kell07@wwu.de>
2022-02-07 14:51:51 +0100

    added sterol flip flop function

--- package/bilana2/analysis/__init__.py
+++ package/bilana2/analysis/__init__.py
@@ -1,3 +1,3 @@
 from .gromacs_analysis import calc_density, calc_rdf, calc_diffusion
-from .interleaflet import calculate_overlapscore, calculate_thickness
+from .interleaflet import calculate_overlapscore, calculate_thickness, calc_sterol_flipflops
 from .misc import get_distance
